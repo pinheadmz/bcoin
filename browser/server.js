08deb2257b2ee03d22f3844c10872f066c766f22
Matthew Zipkin <pinheadmz@gmail.com>
2018-11-13 01:15:03 +0000

    serve four types

--- browser/server.js
+++ browser/server.js
@@ -3,17 +3,18 @@
 const bweb = require('bweb');
 const fs = require('bfile');
 const WSProxy = require('./wsproxy');
+const Validator = require('bval');
+
 
 const index = fs.readFileSync(`${__dirname}/index.html`);
-const app = fs.readFileSync(`${__dirname}/app.js`);
-const worker = fs.readFileSync(`${__dirname}/worker.js`);
 
 const proxy = new WSProxy({
   ports: [8333, 18333, 18444, 28333, 28901]
 });
 
 const server = bweb.server({
-  port: Number(process.argv[2]) || 8080,
+  host: '0.0.0.0',
+  port: 8080,
   sockets: false
 });
 
@@ -31,14 +32,28 @@ server.get('/', (req, res) => {
   res.send(200, index, 'html');
 });
 
-server.get('/app.js', (req, res) => {
-  res.send(200, app, 'js');
+server.get('/:dir/index.html', (req, res) => {
+  const valid = Validator.fromRequest(req);
+  const dir = valid.str('dir');
+  res.send(200, getFile(dir, 'index.html'), 'html');
 });
 
-server.get('/worker.js', (req, res) => {
-  res.send(200, worker, 'js');
+server.get('/:dir/app.js', (req, res) => {
+  const valid = Validator.fromRequest(req);
+  const dir = valid.str('dir');
+  res.send(200, getFile(dir, 'app.js'), 'js');
+});
+
+server.get('/:dir/worker.js', (req, res) => {
+  const valid = Validator.fromRequest(req);
+  const dir = valid.str('dir');
+  res.send(200, getFile(dir, 'worker.js'), 'js');
 });
 
 proxy.attach(server.http);
 
 server.open();
+
+function getFile(dir, file){
+	return fs.readFileSync(`${__dirname}/${dir}/${file}`)
+}
