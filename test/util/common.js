a56e4ca527056c3e765fdde10222c522933ff509
Braydon Fuller <courier@braydon.com>
2018-12-21 11:46:29 -0800

    test: refactor utils

--- test/util/common.js
+++ test/util/common.js
@@ -85,6 +85,20 @@ common.writeTX = function writeTX(name, tx, view) {
   common.writeFile(`${name}-undo.raw`, undoRaw);
 };
 
+common.rimraf = async function(p) {
+  const allowed = new RegExp('^\/tmp\/(.*)$');
+  if (!allowed.test(p))
+    throw new Error(`Path not allowed: ${p}.`);
+
+  return await fs.rimraf(p);
+};
+
+common.sleep = async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
 function parseUndo(data) {
   const br = bio.read(data);
   const items = [];
