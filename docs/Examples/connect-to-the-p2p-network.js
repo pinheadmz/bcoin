2cbe40772748bb78a6d4b90700d6a4fac39df290
Matthew Zipkin <pinheadmz@gmail.com>
2019-02-22 16:17:46 -0800

    examples nits

--- docs/Examples/connect-to-the-p2p-network.js
+++ docs/Examples/connect-to-the-p2p-network.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const bcoin = require('../..').set('main');
 
 const Logger = require('blgr');
--- docs/Examples/create-a-blockchain-and-mempool.js
+++ docs/Examples/create-a-blockchain-and-mempool.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const bcoin = require('../..');
 
 // Default network (so we can avoid passing
