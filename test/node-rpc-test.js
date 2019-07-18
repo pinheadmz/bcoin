29f5b989d5e4069d62342018396a8a7245e0880a
Matthew Zipkin <pinheadmz@gmail.com>
2019-07-18 16:00:20 -0700

    node-rpc: enable opt-in RBF in createrawtransaction
    Adds new optional parameter to rpc createrawtransaction:
    "replaceable" is a boolean that indicates if the TX should signal
    opt-in replaceability by setting at least one input's sequence to
    0xfffffffd. Throws if any user-supplied sequence numbers signal
    RBF when "replaceable" is false.

--- test/node-rpc-test.js
+++ test/node-rpc-test.js
@@ -5,6 +5,8 @@
 
 const assert = require('bsert');
 const FullNode = require('../lib/node/fullnode');
+const Address = require('../lib/primitives/address');
+const TX = require('../lib/primitives/tx');
 
 const ports = {
   p2p: 49331,
@@ -59,5 +61,98 @@ describe('RPC', function() {
   it('should rpc getinfo', async () => {
     const info = await nclient.execute('getinfo', []);
     assert.strictEqual(info.blocks, 0);
+  });
+
+  describe('createrawtransaction', function() {
+    const txid0 = Buffer.alloc(32, 8).toString('hex');
+    const vout0 = 9;
+    const addr0 = new Address();
+    const value0 = 120000;
+    const locktime = 1563489605;
+
+    it('should create a tx with default sequence', async() => {
+      const txHex = await nclient.execute(
+        'createrawtransaction',
+        [
+          [{txid: txid0, vout: vout0}],
+          {[addr0.toString('regtest')]:  value0}
+        ]);
+
+      const tx = TX.fromRaw(Buffer.from(txHex, 'hex'));
+
+      assert.strictEqual(tx.inputs.length, 1);
+      assert.bufferEqual(tx.inputs[0].prevout.hash, txid0);
+      assert.strictEqual(tx.inputs[0].prevout.index, vout0);
+      assert.strictEqual(tx.inputs[0].sequence, 0xffffffff);
+
+      assert.strictEqual(tx.outputs.length, 1);
+      assert.strictEqual(tx.outputs[0].value, value0 * 1e8);
+      assert.deepStrictEqual(tx.outputs[0].getAddress(), addr0);
+    });
+
+    it('should create a tx with locktime', async() => {
+      const txHex = await nclient.execute(
+        'createrawtransaction',
+        [
+          [{txid: txid0, vout: vout0}],
+          {[addr0.toString('regtest')]:  value0},
+          locktime
+        ]);
+
+      const tx = TX.fromRaw(Buffer.from(txHex, 'hex'));
+
+      assert.strictEqual(tx.locktime, locktime);
+
+      assert.strictEqual(tx.inputs.length, 1);
+      assert.bufferEqual(tx.inputs[0].prevout.hash, txid0);
+      assert.strictEqual(tx.inputs[0].prevout.index, vout0);
+      assert.strictEqual(tx.inputs[0].sequence, 0xfffffffe);
+
+      assert.strictEqual(tx.outputs.length, 1);
+      assert.strictEqual(tx.outputs[0].value, value0 * 1e8);
+      assert.deepStrictEqual(tx.outputs[0].getAddress(), addr0);
+    });
+
+    it('should create a tx with opt-in RBF', async() => {
+      const txHex = await nclient.execute(
+        'createrawtransaction',
+        [
+          [{txid: txid0, vout: vout0}],
+          {[addr0.toString('regtest')]:  value0},
+          locktime,
+          true
+        ]);
+
+      const tx = TX.fromRaw(Buffer.from(txHex, 'hex'));
+
+      assert.strictEqual(tx.locktime, locktime);
+
+      assert.strictEqual(tx.inputs.length, 1);
+      assert.bufferEqual(tx.inputs[0].prevout.hash, txid0);
+      assert.strictEqual(tx.inputs[0].prevout.index, vout0);
+      assert.strictEqual(tx.inputs[0].sequence, 0xfffffffd);
+
+      assert.strictEqual(tx.outputs.length, 1);
+      assert.strictEqual(tx.outputs[0].value, value0 * 1e8);
+      assert.deepStrictEqual(tx.outputs[0].getAddress(), addr0);
+
+      assert(tx.isRBF());
+    });
+
+    it('should reject a tx with conflicting sequence', async() => {
+      await assert.rejects(async() => {
+        await nclient.execute(
+          'createrawtransaction',
+          [
+            [{txid: txid0, vout: vout0, sequence: 0xffff0000}],
+            {[addr0.toString('regtest')]:  value0},
+            locktime,
+            false
+          ]);
+      }, {
+        name: 'Error',
+        message: 'Sequence conflicts with replaceability.'
+      });
+    });
   });
 });
